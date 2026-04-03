0e8fb0e7135adb9d7e267c334ab1a5204a5654b6
Ebube Williams <chidiekwebelam@gmail.com>
2026-04-03 16:41:21 +0100

    Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold hidden">Ebube</div>;
+  return <div className="text-sm font-semibold hidden md:block">Ebube</div>;
 }
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-300 sm:px-6">
+    <header className="flex items-center justify-between bg-yellow-500 px-4 py-3 uppercase border-b border-stone-300 sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
