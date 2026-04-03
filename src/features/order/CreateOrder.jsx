85c5a611e15dbc38bb39e6310e402b9c5fca43e4
Ebube Williams <chidiekwebelam@gmail.com>
2026-04-03 18:57:20 +0100

    Reusing Styles With @apply

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,13 +48,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formError?.phone && <p className="error">{formError.phone}</p>}
         </div>
@@ -62,12 +62,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
