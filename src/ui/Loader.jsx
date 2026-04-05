295b93d342d55eab364439031d5abb9445d98bc7
williamsconcepts <chidiekwebelam@gmail.com>
2026-04-05 01:09:24 +0100

    Absolute Positioning, z-index, and More

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="items-centerjustify-center absolute inset-0 flex bg-slate-200/20 backdrop-blur-sm">
+    <div className="items-center justify-center absolute inset-0 flex bg-slate-200/20 backdrop-blur-sm">
       <div className="loader"></div>
     </div>
   );
