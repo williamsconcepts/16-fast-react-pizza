66df9c97645e9b65bc111715bd0219fa8813947e
williamsconcepts <chidiekwebelam@gmail.com>
2026-04-05 01:01:38 +0100

    Reusing Styles With React Components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,7 +9,7 @@ function AppLayout() {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {/* {isLoading && <Loader />} */}
-      {true && <Loader />}
+      {isLoading && <Loader />}
       <Header />
 
       <div className="overflow-scroll">
