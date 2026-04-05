6d4219a08ef4fd70658b96a06cf9ee8e4df27f33
williamsconcepts <chidiekwebelam@gmail.com>
2026-04-05 08:02:17 +0100

    Configuring Tailwind: Custom Font Family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+
     extend: {},
   },
   plugins: [],
