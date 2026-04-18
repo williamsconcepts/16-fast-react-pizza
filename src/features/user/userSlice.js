eeafab048ce49a94679fcbdc4abc6417709a986d
Ebube Williams <chidiekwebelam@gmail.com>
2026-04-18 17:37:27 +0100

    Integrating Geolocation

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -55,7 +55,8 @@ const userSlice = createSlice({
       })
       .addCase(fetchAddress.rejected, (state, action) => {
         state.status = 'error';
-        state.error = action.error.message;
+        state.error =
+          'There was an error getting your address, Make sure to fill this field!';
       }),
 });
 
